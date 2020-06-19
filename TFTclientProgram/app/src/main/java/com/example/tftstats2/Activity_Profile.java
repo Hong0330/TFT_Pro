32275f150ffb7a7462156d42c04a210faf72d291
gitPlayLee <lion7542@naver.com>
2020-06-19 21:06:10 +0900

    명시된 saved와 profile 클래스만 추가

---
+++ TFTclientProgram/app/src/main/java/com/example/tftstats2/Activity_Profile.java
@@ -0,0 +1,28 @@
+package com.example.tftstats2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+// 나의 프로필 클래스
+public class Activity_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup); // UI 변경 필요
+    }
+}
