4cd5c8514129709785310451ff8897d4c5b87460
BuildTools <unconfigured@null.spigotmc.org>
2020-06-19 14:53:32 +0900

    1. 툴바 검색 기능 구현

--- TFTclientProgram/app/src/main/java/com/example/tftstats2/Activity_Toolbar.java
+++ TFTclientProgram/app/src/main/java/com/example/tftstats2/Activity_Toolbar.java
@@ -1,12 +1,15 @@
 package com.example.tftstats2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -45,11 +48,17 @@ public class Activity_Toolbar extends AppCompatActivity {
                 int id = menuItem.getItemId();
                 String title = menuItem.getTitle().toString();
 
-                if(id == R.id.account){
-                    Toast.makeText(context, title + ": 계정 정보를 확인합니다.", Toast.LENGTH_SHORT).show();
+                if(id == R.id.saved){
+                    Intent intent = new Intent(
+                            getApplicationContext(),
+                            Activity_Toolbar.class);
+                    startActivity(intent);
                 }
-                else if(id == R.id.setting){
-                    Toast.makeText(context, title + ": 설정 정보를 확인합니다.", Toast.LENGTH_SHORT).show();
+                else if(id == R.id.profile){
+                    Intent intent = new Intent(
+                            getApplicationContext(),
+                            Activity_Toolbar.class);
+                    startActivity(intent);
                 }
                 else if(id == R.id.logout){
                     Toast.makeText(context, title + ": 로그아웃 시도중", Toast.LENGTH_SHORT).show();
@@ -61,7 +70,27 @@ public class Activity_Toolbar extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+// 검색 버튼 클릭했을 때 searchview 길이 꽉차게 늘려주기
+        SearchView searchView = (SearchView)menu.findItem(R.id.action_search).getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setQueryHint("소환사 검색");
+        return true;
+    }
+
+
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_search) {
+        //검색했을 때 쿼리 구현
+            return true;
+        }
+
         switch (item.getItemId()){
             case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
                 mDrawerLayout.openDrawer(GravityCompat.START);
