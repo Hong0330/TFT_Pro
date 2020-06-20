4d2ebd71047fc16f5812710364c1262d720b64bc
BuildTools <unconfigured@null.spigotmc.org>
2020-06-20 21:51:43 +0900

    이미지 추가, 커스텀 리사이클러뷰 구성

--- TFTclientProgram/app/src/main/java/com/example/tftstats2/Activity_Toolbar.java
+++ TFTclientProgram/app/src/main/java/com/example/tftstats2/Activity_Toolbar.java
@@ -13,13 +13,19 @@ import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Activity_Toolbar extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private Context context = this;
+    private RecyclerAdapter adapter;
 
 
     @Override
@@ -29,7 +35,16 @@ public class Activity_Toolbar extends AppCompatActivity {
 
         // 추가된 소스, Toolbar를 생성한다.
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        RecyclerView recyclerView = findViewById(R.id.matchlist);
         setSupportActionBar(myToolbar);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+        init();
+        getData();
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
@@ -98,5 +113,67 @@ public class Activity_Toolbar extends AppCompatActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void init() {
+        RecyclerView recyclerView = findViewById(R.id.matchlist);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+    private void getData() {
+        // 임의의 데이터
+        List<String> listTitle = Arrays.asList("아리", "애니", "애쉬", "아우렐리온 솔", "소나", "소라카", "신드라", "샤코",
+                "쉔", "다리우스", "피오라", "피즈", "갱플랭크", "가렌", "오공");
+        List<String> listContent = Arrays.asList(
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터.",
+                "데이터."
+        );
+        List<Integer> listResId = Arrays.asList(
+                R.drawable.ahri,
+                R.drawable.annie,
+                R.drawable.ashe,
+                R.drawable.aurelionsol,
+                R.drawable.sona,
+                R.drawable.soraka,
+                R.drawable.syndra,
+                R.drawable.shaco,
+                R.drawable.shen,
+                R.drawable.darius,
+                R.drawable.fiora,
+                R.drawable.fizz,
+                R.drawable.gangplank,
+                R.drawable.garen,
+                R.drawable.wukong
+        );
+        for (int i = 0; i < listTitle.size(); i++) {
+            // 각 List의 값들을 data 객체에 set 해줍니다.
+            Data data = new Data();
+            data.setTitle(listTitle.get(i));
+            data.setContent(listContent.get(i));
+            data.setResId(listResId.get(i));
+
+            // 각 값이 들어간 data를 adapter에 추가합니다.
+            adapter.addItem(data);
+        }
+
+        // adapter의 값이 변경되었다는 것을 알려줍니다.
+        adapter.notifyDataSetChanged();
     }
 }
